49c5fc8e59316d590acd6d80e4af24ed799620b9
Zijia Zeng <zz3tq@virginia.edu>
2019-05-30 12:35:50 -0400

    configure middleware

--- app.js
+++ app.js
@@ -27,6 +27,15 @@ db.connect((err) => {
 });
 global.db = db;
 
+//configure middleware
+app.set('port', process.env.port || port); // set express to use this port
+app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
+app.set('view engine', 'ejs'); // configure template engine
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // parse form data client
+app.use(fileUpload()); // configure fileupload
+
+
 // set the app to listen on the port
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
