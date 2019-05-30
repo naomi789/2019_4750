4b771fc91eddc7e0a280f082c64ec6a7c91ecf4c
naomi <snjohnson789@gmail.com>
2019-05-30 09:42:04 -0400

    change slash direction, commented out checks on vars, commented out kanji field

--- read_in_data_trials.sql
+++ read_in_data_trials.sql
@@ -13,25 +13,24 @@ Primary key and foreign key constraints must be included.
 Check constraints must be included as well where applicable.*/
 
 CREATE TABLE IF NOT EXISTS jmdict(
-  k_ele VARCHAR(20),
+  # k_ele VARCHAR(20),
   ent_seq VARCHAR(20),
   r_ele VARCHAR(20),
-  gloss VARCHAR(20),
-  CONSTRAINT PK_jmdict PRIMARY KEY (k_ele, gloss)
+  gloss VARCHAR(2000),
+  CONSTRAINT PK_jmdict PRIMARY KEY (ent_seq)
 );
 
 ALTER TABLE jmdict CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 /*2. SQL insert statements to fill your tables with initial data. Include enough data to show proper testing of your SQL select statements below.*/
-SHOW VARIABLES LIKE 'local_infile';
-SET GLOBAL local_infile = 1;
+# SHOW VARIABLES LIKE 'local_infile';
+# SET GLOBAL local_infile = 1;
 -- /data/JMdict_e.xml
 -- /data/alt.txt
 -- 'C:\ProgramData\MySQL\MySQL Server 8.0\Uploads\'
+# SHOW VARIABLES LIKE "secure_file_priv";
 
-SHOW VARIABLES LIKE "secure_file_priv";
-
-LOAD XML INFILE "C:\ProgramData\MySQL\MySQL Server 8.0\Uploads\data\JMdict_e.xml"
+LOAD XML INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/data/JMdict_e.xml"
 INTO TABLE jmdict
 ROWS IDENTIFIED BY '<entry>';
 
