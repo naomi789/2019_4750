2dc070a3a6f0362e937b0bfb3d551805969fb628
Zijia Zeng <zz3tq@virginia.edu>
2019-05-30 12:34:08 -0400

    connect port with mysql connection

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-var mysql = require('mysql');
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const path = require('path');
+const app = express();
 
-var con = mysql.createConnection({
+// const {getHomePage} = require('./routes/index');
+// const {addPlayerPage, addPlayer, deletePlayer, editPlayer, editPlayerPage} = require('./routes/player');
+const port = 5000;
+
+//create connection
+const db = mysql.createConnection({
   host: "localhost",
   user: "data",
-  password: "password"
+  password: "password",
+  database: "main_db"
 
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("USE main_db", function (err, result, fields) {
-    if (err) throw err;
-    console.log("Correct");
-  });
-  con.query("SELECT * FROM english_word", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
+// connect to database
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+global.db = db;
+
+// set the app to listen on the port
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
 });
