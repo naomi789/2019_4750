93b09ca7e75356487f5cee2a3a2dc7a5916f4090
naomi <snjohnson789@gmail.com>
2019-05-30 12:33:42 -0400

    trying 'utf8mb4_ja_0900_as_cs' and checking vars; no luck yet

--- read_in_data_trials.sql
+++ read_in_data_trials.sql
@@ -12,17 +12,25 @@ CREATE TABLE IF NOT EXISTS jmdict(
   k_ele VARCHAR(20), # The kanji element, or in its absence, the reading element, is the defining component of each entry.
   CONSTRAINT PK_jmdict PRIMARY KEY (ent_seq)
 );
-
-ALTER TABLE jmdict CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE jmdict CHANGE keb keb VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE jmdict CHANGE ent_seq ent_seq VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE jmdict CHANGE r_ele r_ele VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE jmdict CHANGE pos pos VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE jmdict CHANGE lsource lsource VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; 
-ALTER TABLE jmdict CHANGE gloss gloss VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE jmdict CHANGE k_ele k_ele VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+# utf8mb4_unicode_ci
+ALTER TABLE jmdict CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_ja_0900_as_cs;
+ALTER TABLE jmdict CHANGE keb keb VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_ja_0900_as_cs;
+ALTER TABLE jmdict CHANGE ent_seq ent_seq VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_ja_0900_as_cs;
+ALTER TABLE jmdict CHANGE r_ele r_ele VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_ja_0900_as_cs;
+ALTER TABLE jmdict CHANGE pos pos VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_ja_0900_as_cs;
+ALTER TABLE jmdict CHANGE lsource lsource VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_ja_0900_as_cs; 
+ALTER TABLE jmdict CHANGE gloss gloss VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_ja_0900_as_cs;
+ALTER TABLE jmdict CHANGE k_ele k_ele VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_ja_0900_as_cs;
 
 SHOW VARIABLES LIKE "secure_file_priv";
+SELECT VERSION();
+SELECT CHARACTER_SET_NAME, DESCRIPTION
+       FROM INFORMATION_SCHEMA.CHARACTER_SETS
+       WHERE DESCRIPTION LIKE '%Chin%'
+       OR DESCRIPTION LIKE '%Japanese%'
+       OR DESCRIPTION LIKE '%Korean%'
+       ORDER BY CHARACTER_SET_NAME;
+SHOW VARIABLES LIKE 'char%';
 
 LOAD XML INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/data/JMdict_e.xml"
 INTO TABLE jmdict
