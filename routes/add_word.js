a02b28474f220725fb72f341a4a4d22f24f879f8
Zijia Zeng <zz3tq@virginia.edu>
2019-06-02 19:39:17 -0400

    finished add word to list

--- routes/add_word.js
+++ routes/add_word.js
@@ -1,14 +1,23 @@
-/*
 module.exports = {
     addWordPage: (req, res) => {
 
         //console.log("SELECT * FROM learn_kanji WHERE romaji = " + "'" + req.query.search + "'");
-        console.log("here");
+        console.log("INSERT INTO vocab_list_word VALUES (" + req.query.kanji_id + "," + req.query.english_id + "," + req.query.list + ")");
 
-        let query = "SELECT * FROM join_all_tables WHERE vocab_list_id = " + "'" + req.query.list + "'"; // query database to get all the players
+        let query1 = "INSERT INTO vocab_list_word VALUES (" + req.query.kanji_id + "," + req.query.english_id + "," + req.query.list + ")";
+        let query2 = "SELECT * FROM join_all_tables WHERE vocab_list_id = " + "'" + req.query.list + "'"; // query database to get all the players
 
         // execute query
-        db.query(query, (err, result) => {
+        
+        db.query(query1, (err, result) => {
+            if (err) {
+                res.redirect('/');
+            }
+            //console.log(result[2]);
+
+        });
+
+        db.query(query2, (err, result) => {
             if (err) {
                 res.redirect('/');
             }
@@ -17,16 +26,5 @@ module.exports = {
             });
         });
 
-    },
-};
-*/
-
-module.exports = {
-    addWordPage: (req, res) => {
-        // execute query
-        console.log(req.query.list);
-        res.render('help.ejs', {
-            title: "Welcome to Dictionary | View FAQ"
-        });
     },
 };
