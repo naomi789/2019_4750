f2635a22b2aee1bf632120cfcf233f9f094a1ba5
naomi <snjohnson789@gmail.com>
2019-06-02 12:04:36 -0400

    now does three vocab lists at once

--- input_parsing/vocab_find_ent_seq.py
+++ input_parsing/vocab_find_ent_seq.py
@@ -6,54 +6,72 @@ import collections
 
 jim_file_name = './../data/JMdict_e/JMdict_e.json'
 
-list_id = '0001'
-list_name = 'JLPT N5'
-list_description = 'Vocabulary from Peter van der Woude\\\'s www.jlptstudy.net'
-input_file_name = './../data/vocab/jlpt_vocab_n5.csv'
-output_file_name = 'n5_peter_create.sql'
+def main():
+    list_id = '0001'
+    list_name = 'JLPT N5'
+    list_description = 'Vocabulary from www.jlptstudy.net by Peter van der Woude'
+    input_file_name = './../data/vocab/jlpt_vocab_n5.csv'
+    output_file_name = 'n5_peter_create.sql'
+    do_everything(list_id, list_name, list_description, input_file_name, output_file_name)
 
-with open(input_file_name, 'r') as file:
-    all_data = str.splitlines(file.read())
+    list_id = '0002'
+    list_name = 'JLPT N4'
+    list_description = 'Vocabulary from www.jlptstudy.net by Peter van der Woude'
+    input_file_name = './../data/vocab/jlpt_vocab_n4.csv'
+    output_file_name = 'n4_peter_create.sql'
+    do_everything(list_id, list_name, list_description, input_file_name, output_file_name)
 
-with open(jim_file_name, 'r') as file:
-    jim_data = file.read()
-    jim_data = json.loads(jim_data)
+    list_id = '0003'
+    list_name = 'JLPT N2'
+    list_description = 'Vocabulary from www.jlptstudy.net by Peter van der Woude'
+    input_file_name = './../data/vocab/jlpt_vocab_n2.csv'
+    output_file_name = 'n2_peter_create.sql'
+    do_everything(list_id, list_name, list_description, input_file_name, output_file_name)
 
-with open(output_file_name, 'w') as output:
-    output.write('USE main_db;\n')
-    for entry in all_data:
-        entry_list = [x.strip() for x in entry.split(',')]
-        # 0 = peter's id
-        # 1 = reading
-        # 2 = kanji
-        # 3 = peter's pos
-        # 4 = peter's eng
-        print(entry_list[0])
-        for jim_entry in jim_data:
-            ent_seq = jim_entry['ent_seq'][0]
-            r_ele = jim_entry['r_ele']
-            r_ele = r_ele[0]
-            reb = r_ele['reb'][0]
-            if 'k_ele' in jim_entry:
-                k_ele = jim_entry['k_ele'] # an object, MAY NOT EXIST
-                k_ele = k_ele[0]
-                assert(len(k_ele['keb']) == 1)
-                keb = k_ele['keb'][0]
-            else:
-                keb = None
 
-            jim_reading = reb
-            jim_kanji = keb
+def do_everything(list_id, list_name, list_description, input_file_name, output_file_name):
+    with open(input_file_name, 'r') as file:
+        all_data = str.splitlines(file.read())
 
-            if jim_kanji is not None:
-                if entry_list[1] == jim_reading and entry_list[2] == jim_kanji:
-                    # then it's a match
-                    print('match!')
-                    output.write(str('INSERT INTO list_word VALUES(\'' + ent_seq + '\',\'' + list_id + '\');\n'))
-                    output.write(str('INSERT INTO list VALUES(\'' + list_id + '\',' + list_name + '\',\'' + list_description + '\');\n'))
-            else:
-                if entry_list[1] == jim_reading:
-                    # then it's a match
-                    print('match!')
-                    output.write(str('INSERT INTO list_word VALUES(\'' + ent_seq + '\',\'' + list_id + '\');\n'))
-                    output.write(str('INSERT INTO list VALUES(\'' + list_id + '\',' + list_name + '\',\'' + list_description + '\');\n'))
+    with open(jim_file_name, 'r') as file:
+        jim_data = file.read()
+        jim_data = json.loads(jim_data)
+
+    with open(output_file_name, 'w') as output:
+        output.write('USE main_db;\n')
+        for entry in all_data:
+            entry_list = [x.strip() for x in entry.split(',')]
+            # 0 = peter's id
+            # 1 = reading
+            # 2 = kanji
+            # 3 = peter's pos
+            # 4 = peter's eng
+            print(list_name, entry_list[0])
+            for jim_entry in jim_data:
+                ent_seq = jim_entry['ent_seq'][0]
+                r_ele = jim_entry['r_ele']
+                r_ele = r_ele[0]
+                reb = r_ele['reb'][0]
+                if 'k_ele' in jim_entry:
+                    k_ele = jim_entry['k_ele'] # an object, MAY NOT EXIST
+                    k_ele = k_ele[0]
+                    assert(len(k_ele['keb']) == 1)
+                    keb = k_ele['keb'][0]
+                else:
+                    keb = None
+
+                jim_reading = reb
+                jim_kanji = keb
+
+                if jim_kanji is not None:
+                    if entry_list[1] == jim_reading and entry_list[2] == jim_kanji:
+                        # then it's a match
+                        print('match!')
+                        output.write(str('INSERT INTO list_word VALUES(\'' + ent_seq + '\',\'' + list_id + '\');\n'))
+                        output.write(str('INSERT INTO list VALUES(\'' + list_id + '\',' + list_name + '\',\'' + list_description + '\');\n'))
+                else:
+                    if entry_list[1] == jim_reading:
+                        # then it's a match
+                        print('match!')
+                        output.write(str('INSERT INTO list_word VALUES(\'' + ent_seq + '\',\'' + list_id + '\');\n'))
+                        output.write(str('INSERT INTO list VALUES(\'' + list_id + '\',' + list_name + '\',\'' + list_description + '\');\n'))
