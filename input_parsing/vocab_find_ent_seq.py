ea324cb9fc789a82eab9483768977248d5876205
naomi <snjohnson789@gmail.com>
2019-06-02 11:47:09 -0400

    deleting prints, appears to be running correctly

--- input_parsing/vocab_find_ent_seq.py
+++ input_parsing/vocab_find_ent_seq.py
@@ -6,6 +6,7 @@ import collections
 
 jim_file_name = './../data/JMdict_e/JMdict_e.json'
 
+list_id = '0001'
 list_name = 'JLPT N5'
 list_description = 'Vocabulary from Peter van der Woude\'s www.jlptstudy.net'
 input_file_name = './../data/vocab/jlpt_vocab_n5.csv'
@@ -21,27 +22,46 @@ with open(jim_file_name, 'r') as file:
 with open(output_file_name, 'w') as output:
     output.write('USE main_db;\n')
     for entry in all_data:
-        entry_list = entry.split(',')
+        entry_list = [x.strip() for x in entry.split(',')]
         # 0 = peter's id
         # 1 = reading
         # 2 = kanji
         # 3 = peter's pos
         # 4 = peter's eng
-
+        print(entry_list[0])
+        # print(entry_list[0], 'looking for word: \'', entry_list[1], entry_list[2], '\' in the dictionary')
         for jim_entry in jim_data:
-            r_ele = entry['r_ele']
+            ent_seq = jim_entry['ent_seq'][0]
+            r_ele = jim_entry['r_ele']
             r_ele = r_ele[0]
-            reb = r_ele['reb']
-            if 'k_ele' in entry:
-                k_ele = entry['k_ele'] # an object, MAY NOT EXIST
+            reb = r_ele['reb'][0]
+            if 'k_ele' in jim_entry:
+                k_ele = jim_entry['k_ele'] # an object, MAY NOT EXIST
                 k_ele = k_ele[0]
-                keb = k_ele['keb']
+                assert(len(k_ele['keb']) == 1)
+                keb = k_ele['keb'][0]
             else:
                 keb = None
 
             jim_reading = reb
             jim_kanji = keb
-            if entry_list[1] == jim_reading and entry_list[2] == jim_kanji:
-                # then it's a match
-                output.write(str('INSERT INTO list_word VALUES(\'' + ent_seq + '\',\'' + list_id + '\');'))
-                output.write(str('INSERT INTO list VALUES(\'' + list_id + '\'' + list_name + list_description);'))
+
+            # print(ent_seq, jim_reading, jim_kanji)
+
+            if jim_kanji is not None:
+                if entry_list[1] == jim_reading and entry_list[2] == jim_kanji:
+                    # then it's a match
+                    print('match!')
+                    output.write(str('INSERT INTO list_word VALUES(\'' + ent_seq + '\',\'' + list_id + '\');\n'))
+                    output.write(str('INSERT INTO list VALUES(\'' + list_id + '\',' + list_name + '\',\'' + list_description + '\');\n'))
+            else:
+                if entry_list[1] == jim_reading:
+                    # then it's a match
+                    print('match!')
+                    output.write(str('INSERT INTO list_word VALUES(\'' + ent_seq + '\',\'' + list_id + '\');\n'))
+                    output.write(str('INSERT INTO list VALUES(\'' + list_id + '\',' + list_name + '\',\'' + list_description + '\');\n'))
+
+            #     output.write(jim_reading)
+                # print(jim_kanji)
+                # print(str(jim_reading + jim_kanji))
+        # output.write(str('\'' + entry_list[1] + '\', \'' + entry_list[2] + '\''))
