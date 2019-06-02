2b8fd5dd9d97e1ac9b7334cce50835be2135b0d8
Zijia Zeng <zz3tq@virginia.edu>
2019-06-02 18:19:13 -0400

    two hidden inputs and one shown input working

--- routes/detail.js
+++ routes/detail.js
@@ -2,7 +2,7 @@ module.exports = {
     getDetailPage: (req, res) => {
 
         //console.log(req.params);
-        let query = "SELECT * FROM learn_kanji WHERE romaji = " + "'" + req.params.romaji + "'"; 
+        let query = "SELECT * FROM join_all_tables WHERE romaji = " + "'" + req.params.romaji + "'"; 
 
         // execute query
         db.query(query, (err, result) => {
