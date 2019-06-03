ea854ddf5d833a10fc5d4ae9943251713da2393e
Zijia Zeng <zz3tq@virginia.edu>
2019-06-03 14:15:01 -0400

    add id and ent_seq to brief_result view

--- views/views.sql
+++ views/views.sql
@@ -1,8 +1,9 @@
 /*create views*/
 
 /*view used for showing result after search*/
+DROP VIEW brief_result; 
 CREATE VIEW brief_result AS
-SELECT jmdict.keb, jmdict.reb, gloss.gloss_def
+SELECT gloss_word.gloss_ID, gloss_word.ent_seq, jmdict.keb, jmdict.reb, gloss.gloss_def 
 FROM  gloss_word
 LEFT JOIN gloss 
 ON gloss_word.gloss_ID = gloss.gloss_ID
