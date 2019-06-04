fe04f7d6491f29b4c7bb9b8742e7cdfd0c1cb2fe
Zijia Zeng <zz3tq@virginia.edu>
2019-06-04 07:48:49 -0400

    fix ent_seq undefined error

--- routes/add_word.js
+++ routes/add_word.js
@@ -1,18 +1,19 @@
 module.exports = {
     addWordPage: (req, res) => {
 
-        let query1 = "INSERT IGNORE INTO list_word VALUES (" + req.query.ent_seq + "," + req.query.list_name + ")";
+        //let query1 = "INSERT IGNORE INTO list_word VALUES (" + req.query.ent_seq + "," + req.query.list_name + ")";
+        console.log(req.query.ent_seq);
         let query2 = "SELECT * FROM words_in_lists WHERE list_name = " + "'" + req.query.list_name + "'"; // query database to get all the players
 
         // execute query
-
+/*
         db.query(query1, (err, result) => {
             if (err) {
                 return res.redirect('/');
             }
             //console.log(result[2]);
 
-        });
+        });*/
 
         db.query(query2, (err, result) => {
             if (err) {
