b04bf0c14ff29ee1185af5329756312699f32d82
Luke Wolpert <lrw5pu@virginia.edu>
2019-06-04 15:21:37 -0400

    regex search optimization non functional

--- routes/search.js
+++ routes/search.js
@@ -1,8 +1,20 @@
 module.exports = {
     getSearchPage: (req, res) => {
-        
-        //let query = "SELECT * FROM join_all_tables WHERE romaji = " + "'" + req.query.search + "'"; // query database to get all the players
-        let query = "SELECT * FROM brief_result WHERE keb LIKE '" + req.query.search + "%' OR gloss_def LIKE '" + req.query.search + "%' OR reb LIKE '" + req.query.search + "%'";
+                
+        var regex = /[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B/g; 
+        var 
+        if(regex.test(req.query.search)) {
+            console.log("SELECT * FROM brief_result WHERE keb LIKE '" + req.query.search + "%' OR reb LIKE '" + req.query.search + "%'");   
+            let query = "SELECT * FROM brief_result WHERE keb LIKE '" + req.query.search + "%' OR reb LIKE '" + req.query.search + "%'";
+        }
+        else {
+            
+            console.log("SELECT * FROM brief_result WHERE gloss_def LIKE '" + req.query.search + "%'");
+            let query = "SELECT * FROM brief_result WHERE gloss_def LIKE '" + req.query.search + "%'";
+        }
+
+
+       //let query = "SELECT * FROM brief_result WHERE keb LIKE '" + req.query.search + "%' OR gloss_def LIKE '" + req.query.search + "%' OR reb LIKE '" + req.query.search + "%'";
         // execute query
         db.query(query, (err, result) => {
             if (err) {
