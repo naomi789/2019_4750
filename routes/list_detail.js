baba955c080499000cdbce6527c7810896b26f37
Luke Wolpert <lrw5pu@virginia.edu>
2019-06-05 03:34:50 -0400

    created clean_pos.sql, which, when run, updates all of your pos_type entries into a readable form

--- routes/list_detail.js
+++ routes/list_detail.js
@@ -1,7 +1,7 @@
 module.exports = {
     getListDetailPage: (req, res) => {
         //console.log("SELECT list_name, list_description, keb, reb, gloss_def, is_foreign, orig_lang, orig_word, waseieigo, pos_type FROM words_in_lists WHERE list_name = " + "'" + req.query.list_name + "'");
-        let query = "SELECT * FROM words_in_lists WHERE list_name = " + "'" + req.params.list_name + "'"; // query database to get all the players
+        let query = "SELECT JMdict.keb, JMdict.reb, gloss.gloss_def, pos.pos_type, list.list_name, list.list_description FROM words_in_lists WHERE list_name = " + "'" + req.params.list_name + "'"; // query database to get all the players
 
         // execute query
         db.query(query, (err, result) => {
